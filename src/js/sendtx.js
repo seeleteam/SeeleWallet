a7cb4dde2afca393e7891b04af2235f98c92110d
muyiy <muyi.yang@biola.edu>
2019-09-11 11:54:08 -0700

    refresh value

--- src/js/sendtx.js
+++ src/js/sendtx.js
@@ -137,6 +137,9 @@ function sendtx() {
     document.getElementById("accountpassWord").value='';
     document.getElementById("amount").value='';
     document.getElementById("to").value='';
+    document.getElementById("txamount1").value='0.00';
+    document.getElementById("txamount1").value='0';
+    document.getElementById("totalamount").value='0.0';
 
 }
 
